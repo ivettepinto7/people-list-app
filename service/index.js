8b67cfe4edf407f6d0c07a0a9250f1631cafc09c
ivettepinto7 <00186319@uca.edu.sv>
2021-11-17 00:54:25 -0600

    add: cors

--- service/index.js
+++ service/index.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const peopleRoutes = require('./routes/PersonRouter')
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/people', peopleRoutes);
 
