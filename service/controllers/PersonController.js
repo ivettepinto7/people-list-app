cc073429a3fdfdac7a9e53f9a47dcf7c5bd6db7b
ivettepinto7 <00186319@uca.edu.sv>
2021-11-18 09:22:09 -0600

    fix: typo

--- service/controllers/PersonController.js
+++ service/controllers/PersonController.js
@@ -9,14 +9,12 @@ var PersonController = {
             
             await person.save();
 
-            return res.json({
-                status: "success",
+            return res.(201).json({
                 data: person,
                 message: "Person added"});
         } catch (err) {
             console.error(err);
-            return res.json({
-                status: "error",
+            return res.status(400).json({
                 data: null,
                 message: err
             });
@@ -25,18 +23,19 @@ var PersonController = {
     updatePerson: async (req, res) => {
         try {
             var actualPerson = await Person.findOne(req.params.id);
+            
             actualPerson = {
-                name: req.body.newName
+                name: req.body.name
             }
+
             await Person.findOneAndUpdate(req.params.id,actualPerson);
-            return res.json({
-                status: "success",
-                data: newPerson,
+            
+            return res.status(200).json({
+                data: actualPerson,
                 message: "Person updated"});
         } catch (err) {
             console.error(err);
-            return res.json({
-                status: "error",
+            return res.status(400).json({
                 data: null,
                 message: err
             });
@@ -45,15 +44,14 @@ var PersonController = {
     deletePerson: async (req, res) => {
         try {
             const deletedPerson = await Person.findByIdAndRemove(req.params.id);
-            return res.json({
-                status: "success",
+            
+            return res.status(200).json({
                 data: deletedPerson,
                 message: "Person deletede"
             });
         } catch (err) {
             console.error(err);
-            return res.json({
-                status: "error",
+            return res.status(200).json({
                 data: null,
                 message: err
             });
@@ -62,14 +60,13 @@ var PersonController = {
     getAllPeople: async (req, res) => {
         try{
             const people = await Person.find();
-            return res.json({
-                status: "success",
+            
+            return res.status(200).json({
                 data: people,
                 message: "People list obtained"
             })
         }catch(err){
-            return res.json({
-                status: "error",
+            return res.status(400).json({
                 data: null,
                 message: err
             });
